1e0d308d34e14a33fcafca15ba2c44edc17d2a1a
RakibRahman <md.rr.talukder@gmail.com>
2021-07-08 19:49:17 +0600

    first project

---
+++ project_zero/src/articles/Posts.js
@@ -0,0 +1,70 @@
+import "./Posts.css";
+import logo from "./../logo.svg";
+
+function Article() {
+  return (
+    <div className="article__body">
+      <div className="article__card">
+        <img src={logo} className="App-logo" alt="logo" />
+
+        <h1>Title</h1>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nihil minima
+          distinctio a excepturi nobis hic perferendis soluta quibusdam ex id
+          eos adipisci expedita modi in, ea aut necessitatibus quas iste.
+        </p>
+      </div>
+      <div className="article__card">
+        <img src={logo} className="App-logo" alt="logo" />
+
+        <h1>Title</h1>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nihil minima
+          distinctio a excepturi nobis hic perferendis soluta quibusdam ex id
+          eos adipisci expedita modi in, ea aut necessitatibus quas iste.
+        </p>
+      </div>
+      <div className="article__card">
+        <img src={logo} className="App-logo" alt="logo" />
+
+        <h1>Title</h1>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nihil minima
+          distinctio a excepturi nobis hic perferendis soluta quibusdam ex id
+          eos adipisci expedita modi in, ea aut necessitatibus quas iste.
+        </p>
+      </div>
+      <div className="article__card">
+        <img src={logo} className="App-logo" alt="logo" />
+
+        <h1>Title</h1>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nihil minima
+          distinctio a excepturi nobis hic perferendis soluta quibusdam ex id
+          eos adipisci expedita modi in, ea aut necessitatibus quas iste.
+        </p>
+      </div>
+      <div className="article__card">
+        <img src={logo} className="App-logo" alt="logo" />
+
+        <h1>Title</h1>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nihil minima
+          distinctio a excepturi nobis hic perferendis soluta quibusdam ex id
+          eos adipisci expedita modi in, ea aut necessitatibus quas iste.
+        </p>
+      </div>
+      <div className="article__card">
+        <img src={logo} className="App-logo" alt="logo" />
+
+        <h1>Title</h1>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nihil minima
+          distinctio a excepturi nobis hic perferendis soluta quibusdam ex id
+          eos adipisci expedita modi in, ea aut necessitatibus quas iste.
+        </p>
+      </div>
+    </div>
+  );
+}
+export default Article;
