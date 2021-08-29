e7210e266459b863dd06a41a6c6560c05b0aead4
RakibRahman <md.rr.talukder@gmail.com>
2021-08-29 19:05:43 +0600

    get new data onclick

--- practice-three/src/Jokes.js
+++ practice-three/src/Jokes.js
@@ -2,7 +2,7 @@
 
 import React from "react";
 import axios from "axios";
-
+import "./Joke.css";
 class Jokes extends React.Component {
   constructor(props) {
     super(props);
@@ -19,14 +19,34 @@ class Jokes extends React.Component {
       console.log(this.state.data);
     });
   }
+
   render() {
     return (
       <div>
         <h1>Joke</h1>
-        <img src={this.state.data[1]} alt="icon" />
-        <p>{this.state.data[0]}</p>
+        <img className="icon" src={this.state.data[1]} alt="icon" />
+        <p className="joke">{this.state.data[0]}</p>
+        <h1>{this.state.reqKey}</h1>
       </div>
     );
   }
 }
-export default Jokes;
+class JokeMania extends React.Component {
+  state = {
+    reqKey: Math.random(),
+  };
+  handleClick = () => {
+    this.setState({ reqKey: Math.random() });
+    console.log(this.state.reqKey);
+  };
+  render() {
+    return (
+      <div className="joke_wrapper">
+        <Jokes key={this.state.reqKey} />
+
+        <button onClick={this.handleClick}>Reload</button>
+      </div>
+    );
+  }
+}
+export default JokeMania;
