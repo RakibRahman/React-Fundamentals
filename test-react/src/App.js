b0eb16edff1867a437063ada4e5bb0c01e630082
RakibRahman <md.rr.talukder@gmail.com>
2021-09-11 21:33:21 +0600

    sort & filter task items

--- test-react/src/App.js
+++ test-react/src/App.js
@@ -1,14 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import uniqid from "uniqid";
 import "./App.css";
 import AddItem from "./AddItem";
 import SearchItem from "./SearchItem";
 import TaskItem from "./TaskItem";
+import SortItems from "./SortItems";
 function App() {
   const data = JSON.parse(localStorage.getItem("taskList")) || [];
   const [items, setItems] = useState(data);
   const [newTask, setNewTask] = useState("");
   const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    const left = [...items].filter((item) => item.checked === false);
+
+    document.title = `Task List || ${left.length} ${
+      left.length === 1 ? " Task" : " Tasks"
+    } To Complete`;
+  }, [items]);
+
   const saveAndSetTask = (taskList) => {
     setItems(taskList);
     localStorage.setItem("taskList", JSON.stringify(taskList));
@@ -33,31 +43,6 @@ function App() {
     saveAndSetTask(taskList);
   };
 
-  const onChangeSort = (e) => {
-    if (e.target.value === "a-z") {
-      const taskList = [...items].sort((a, b) => {
-        if (a.task > b.task) return 1;
-        return 0;
-      });
-      saveAndSetTask(taskList);
-    }
-    if (e.target.value === "z-a") {
-      const taskList = [...items].sort((a, b) => {
-        if (a.task < b.task) return 1;
-        return 0;
-      });
-      saveAndSetTask(taskList);
-    }
-    if (e.target.value === "completed") {
-      const taskList = [...items].sort((a, b) => b.checked - a.checked);
-      saveAndSetTask(taskList);
-    }
-    if (e.target.value === "uncompleted") {
-      const taskList = [...items].sort((a, b) => a.checked - b.checked);
-      saveAndSetTask(taskList);
-    }
-    console.log(items);
-  };
   //! on form submit
   const onSubmitHandler = (e) => {
     e.preventDefault();
@@ -76,15 +61,8 @@ function App() {
       />
       <SearchItem search={search} setSearch={setSearch} />
 
-      <form onSubmit={(e) => e.preventDefault()}>
-        <select name="sortTask" id="sortTask" onChange={onChangeSort}>
-          <option value="sort">Sort Task By...</option>
-          <option value="a-z">↗ A-Z</option>
-          <option value="z-a">↖ Z-A</option>
-          <option value="completed">Completed</option>
-          <option value="uncompleted">Uncompleted</option>
-        </select>
-      </form>
+      <SortItems items={items} setItems={setItems} />
+
       <TaskItem
         items={items.filter((item) =>
           item.task.toLowerCase().includes(search.toLowerCase())
---
+++ test-react/src/SortItems.js
@@ -0,0 +1,45 @@
+import React from "react";
+
+function SortItems({ items, setItems }) {
+  const saveAndSetTask = (taskList) => {
+    setItems(taskList);
+    localStorage.setItem("taskList", JSON.stringify(taskList));
+  };
+  const onChangeSort = (e) => {
+    if (e.target.value === "a-z") {
+      const taskList = [...items].sort((a, b) => {
+        if (a.task > b.task) return 1;
+        return 0;
+      });
+      saveAndSetTask(taskList);
+    }
+    if (e.target.value === "z-a") {
+      const taskList = [...items].sort((a, b) => {
+        if (a.task < b.task) return 1;
+        return 0;
+      });
+      saveAndSetTask(taskList);
+    }
+    if (e.target.value === "completed") {
+      const taskList = [...items].sort((a, b) => b.checked - a.checked);
+      saveAndSetTask(taskList);
+    }
+    if (e.target.value === "uncompleted") {
+      const taskList = [...items].sort((a, b) => a.checked - b.checked);
+      saveAndSetTask(taskList);
+    }
+  };
+  return (
+    <form onSubmit={(e) => e.preventDefault()}>
+      <select name="sortTask" id="sortTask" onChange={onChangeSort}>
+        <option value="sort">Sort Task By...</option>
+        <option value="a-z">↗ A-Z</option>
+        <option value="z-a">↖ Z-A</option>
+        <option value="completed">Completed</option>
+        <option value="uncompleted">Uncompleted</option>
+      </select>
+    </form>
+  );
+}
+
+export default SortItems;
