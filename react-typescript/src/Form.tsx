fa91eea0bd57855aace0896c6bac1615cb452fc9
RakibRahman <md.rr.talukder@gmail.com>
2021-10-29 20:25:30 +0600

    forms

--- react-typescript/src/Form.tsx
+++ react-typescript/src/Form.tsx
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 interface FormFields {
   email: HTMLInputElement;
   message: HTMLInputElement;
 }
-
+interface InitialState {
+  email: string;
+  message: string;
+}
+type FormState = InitialState[];
 export const Form = () => {
+  const initState = {
+    email: "",
+    message: "",
+  };
+  const [formState, setFormState] = useState<InitialState>(initState);
+
+  const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+
+    setFormState({ ...formState, [e.currentTarget.name]: value });
+  };
+
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
     const target = e.target as typeof e.target & FormFields;
     let mail = target.email;
     console.log(mail);
@@ -16,7 +33,13 @@ export const Form = () => {
       <form onSubmit={handleSubmit}>
         <label htmlFor="email">
           Email:
-          <input type="email" name="email" placeholder="enter email address" />
+          <input
+            type="text"
+            name="email"
+            placeholder="enter email address"
+            value={formState.email}
+            onChange={onChangeHandler}
+          />
         </label>
         <label htmlFor="message">
           Message:
@@ -24,9 +47,16 @@ export const Form = () => {
             type="message"
             name="message"
             placeholder="enter your message"
+            value={formState.message}
+            onChange={onChangeHandler}
           />
         </label>
+        <button type="submit">Submit</button>
       </form>
+      <div>
+        <p>{formState.email}</p>
+        <p>{formState.message}</p>
+      </div>
     </div>
   );
 };
