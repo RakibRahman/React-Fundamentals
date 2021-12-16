9d33da94999a9029f90d0781cd05ffdd189dc1af
RakibRahman <md.rr.talukder@gmail.com>
2021-12-16 20:59:46 +0600

    hooks

--- react-hooks-v2/src/rules-of-hooks/Counter.tsx
+++ react-hooks-v2/src/rules-of-hooks/Counter.tsx
@@ -1,9 +1,16 @@
 /* eslint-disable react-hooks/rules-of-hooks */
-import React, { ReactElement, useRef, useState } from "react";
+import React, { ReactElement, useEffect, useRef, useState } from "react";
 
 export function Counter(): ReactElement {
   const [counter, setCounter] = useState(0);
+  const btnRef = useRef<HTMLButtonElement>(null);
 
+  useEffect(() => {
+    setTimeout(() => {
+      console.log("button focused");
+      btnRef.current?.focus();
+    }, 1100);
+  }, []);
   //   for (let index = 0; index < counter; index++) {
   //     useState(true);
   //   }
@@ -18,7 +25,9 @@ export function Counter(): ReactElement {
     <div>
       <div>Count: {counter}</div>
       <div>
-        <button onClick={() => setCounter(counter + 1)}>Increment</button>
+        <button ref={btnRef} onClick={() => setCounter(counter + 1)}>
+          Increment
+        </button>
       </div>
     </div>
   );
